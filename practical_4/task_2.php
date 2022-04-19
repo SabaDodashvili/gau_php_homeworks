49b986561c10f73158b5b4360dd89ddb87ea6539
Saba Dodashvili <sabadodashvili777@gmail.com>
2022-04-19 09:07:08 +0400

    add task 2

--- practical_4/task_2.php
+++ practical_4/task_2.php
@@ -5,11 +5,34 @@
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Document</title>
+  <link rel="stylesheet" href="style.css">
+  <title>task_2</title>
 </head>
 
 <body>
 
+  <form action="" class="form" method="POST">
+    <h1 class="form__title">task-2</h1>
+    <div class="form__item form-item">
+      <input class="form-item__input" type="text" name="name" placeholder=" " />
+      <label class="form-item__label">Name</label>
+    </div>
+    <div class="form__item form-item">
+      <input class="form-item__input" type="datetime-local" name="dateTime" placeholder=" " />
+      <label class="form-item__label">birthday</label>
+    </div>
+    <button class="form__button" type="submit">create file</button>
+  </form>
+
+  <?php
+  if (isset($_POST['name'])) {
+    $fileName = preg_replace('/:/', '-', date('Y-m-d H:i:s') . '.txt');
+    $myFile = fopen($fileName, 'w');
+    fwrite($myFile, $_POST['name'] . ' ',);
+    fwrite($myFile, $_POST['dateTime'],);
+    fclose($myFile);
+  }
+  ?>
 </body>
 
 </html>
